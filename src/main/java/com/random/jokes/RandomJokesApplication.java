591b4257b0b29fa54e849416b50e50f657cc02cc
paritoshP <paritosh.pradeep@rediffmail.com>
2018-11-08 17:11:52 +0530

    CID000----BASE PROJECT

---
+++ src/main/java/com/random/jokes/RandomJokesApplication.java
@@ -0,0 +1,12 @@
+package com.random.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomJokesApplication.class, args);
+    }
+}
